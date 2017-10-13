21fbbd112551db541ad9008403d21013538acc38
Lenyn Alcantara <nightzpy@gmail.com>
2017-10-13 09:50:24 +0000

    Add link to new thread on navbar and update form with channel_id select

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        <li><a href="/threads">All Threads</a></li>
+                        <li><a href="{{ route('threads.index') }}">All Threads</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Channels <span class="caret"></span></a>
                             <ul class="dropdown-menu">
@@ -45,6 +45,7 @@
                                 @endforeach
                             </ul>
                         </li>
+                        <li><a href="{{ route('threads.create') }}">New thread</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
