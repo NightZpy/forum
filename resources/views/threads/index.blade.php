3ede1ec0a1434a07d47f4773d1ef7a487db1d5b2
Lenyn Alcantara <nightzpy@gmail.com>
2017-10-16 10:54:49 +0000

    Fix ui positions for reply count in the threads index

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -1,22 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Threads</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Threads</div>
 
-                <div class="panel-body">
-                    @foreach ($threads as $thread)
-                        <article>
-                            <h4><a href="{{ $thread->path }}">{{ $thread->title }}</a></h4>
-                            <div class="body">{{ $thread->body }}</div>
-                        </article>
-                    @endforeach
+                    <div class="panel-body">
+                        @foreach ($threads as $thread)
+                            <article>
+                                <div class="level">
+                                    <h4 class="flex">
+                                        <a href="{{ $thread->path }}">{{ $thread->title }}</a>
+                                    </h4>
+                                    <strong>
+                                        <a href="{{ $thread->path }}">
+                                            {{ $thread->repliesCountTxt }}
+                                        </a>
+                                    </strong>
+                                </div>
+                                <div class="body">{{ $thread->body }}</div>
+                            </article>
+                            <hr>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
